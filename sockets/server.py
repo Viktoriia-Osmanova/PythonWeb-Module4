2665ed64999b705292afbdbb553a365b6923561c
Victoriia Osmanova <v.osmanova@draftkings.com>
2023-11-23 15:51:02 +0100

    HW

---
+++ sockets/server.py
@@ -0,0 +1,23 @@
+import socket
+
+
+def main():
+    host = socket.gethostname()
+    port = 5000
+
+    server_socket = socket.socket()
+    server_socket.bind((host, port))
+    server_socket.listen(2)
+    conn, address = server_socket.accept()
+    print(f'Connection from {address}')
+    while True:
+        data = conn.recv(100).decode()
+
+        if not data:
+            break
+        print(f'received message: {data}')
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
